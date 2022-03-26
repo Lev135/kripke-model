9c5ff312c1bc7926a90ef96000763d483baecf61
Lev135 <lev_135@mail.ru>
2022-03-26 10:30:16 +0300

    Grid J-models realized

--- src/Main.hs
+++ src/Main.hs
@@ -8,33 +8,34 @@ import Kripke
 import Data.Text (Text)
 import Data.Text.IO
 import Text.Megaparsec (parse, errorBundlePretty, MonadParsec (eof))
-import Text.Megaparsec.Char (string)
-import Control.Applicative (Alternative((<|>)))
+import Text.Megaparsec.Char (string, space1)
+import Control.Applicative (Alternative((<|>), many))
 import Control.Monad (forM_)
+import Text.Megaparsec.Char.Lexer (decimal)
 
-data Relation = R1 | R2
+data Relation = R1 | R0 | R
     deriving Eq
-data Var = A | B | C
-    deriving Eq
+
+type Var = (Int, Int)
 
 instance Show Relation where
+    show R0 = "0"
     show R1 = "1"
-    show R2 = "2"
-
-instance Show Var where
-    show A = "A"
-    show B = "B"
-    show C = "C"
+    show R  = ""
 
 type Formula = MLFormula Var Relation
 
 pVar :: Parser Var
-pVar =  A <$ string "A"
-    <|> B <$ string "B"
-    <|> C <$ string "C"
+pVar =  do
+    string "{"
+    l <- decimal
+    many space1 
+    w <- decimal
+    string "}"
+    return (l, w)
 
 pRel :: Parser Relation
-pRel = R1 <$ string "1" <|> R2 <$ string "2"
+pRel = R0 <$ string "0" <|> R1 <$ string "1" <|> return R
 
 pForm :: Parser (MLFormula Var Relation)
 pForm = pFormula pVar pRel
@@ -55,19 +56,18 @@ ioParseAll p t = case parseAll p t of
 ioParseForm :: Text -> IO (MLFormula Var Relation)
 ioParseForm = ioParseAll pForm
 
-ws = [(a, b) | a <- [1 .. 5], b <- [1 .. 5]]
+ws = [(a, b) | a <- [0 .. 5], b <- [0 .. 5]]
 
 model :: Model (Int, Int) Relation Var
 model = Model {
         rels,  worlds = ws, propEv
     }
     where
-        rels R1 (a, _) (b, _) = a > b
-        rels R2 (_, a) (_, b) = a > b
+        rels R0 (l, _) (l', _)  = l > l'
+        rels R1 (l, w) (l', w') = l == l' && w > w'
+        rels R  (l, w) (l', w') = l > l' && w' /= 0 || l == l' && w > w'
 
-        propEv A = \(a, _) -> a == 1
-        propEv B = \(_, b) -> b == 1
-        propEv C = \(a, b) -> a == 1 && b == 1
+        propEv = (==)
 
 main :: IO ()
 main = do
