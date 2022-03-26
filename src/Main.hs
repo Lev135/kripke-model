8fc49ad04a8d029806f0a9e70f76b69300fedd8a
Lev135 <lev_135@mail.ru>
2022-03-26 08:36:50 +0300

    Parsing Modal logic realized

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,15 @@
 module Main where
 
 import ModalLogic
+import Data.Text (Text)
+import Text.Megaparsec (parse, errorBundlePretty, MonadParsec (eof))
+import Text.Megaparsec.Char (string)
+import Control.Applicative (Alternative((<|>)))
 
 data Relation = R1 | R2
+    deriving Eq
 data Var = A | B | C
+    deriving Eq
 
 instance Show Relation where
     show R1 = "1"
@@ -22,6 +28,29 @@ box1 = Box R1
 trans1A :: Formula
 trans1A = box1 (box1 $ Prop A) *-> box1 (Prop A)
 
+pVar :: Parser Var
+pVar =  A <$ string "A"
+    <|> B <$ string "B"
+    <|> C <$ string "C"
+
+pRel :: Parser Relation
+pRel = R1 <$ string "1" <|> R2 <$ string "2"
+
+pForm :: Parser (MLFormula Var Relation)
+pForm = pFormula pVar pRel 
+
+parsePart :: Parser a -> Text -> Either String a
+parsePart p s = case parse p "" s of
+    Left  e -> Left $ errorBundlePretty e
+    Right a -> return a
+
+parseAll :: Parser a -> Text -> Either String a
+parseAll p = parsePart (p <* eof)
+
+
 main :: IO ()
 main = do
-    print trans1A
+    a <-case parseAll pForm "[1][1]A -> [1]A" of
+        Left  e -> fail e
+        Right a -> return a
+    print $ trans1A == a
--- src/ModalLogic.hs
+++ src/ModalLogic.hs
@@ -1,9 +1,22 @@
+{-# LANGUAGE StandaloneDeriving #-}
 module ModalLogic (
     Op(..), MLFormula(..),
-    (*&), (*|), (*->)
+    (*&), (*|), (*->),
+    Parser, pFormula
 ) where
 
+import Text.Megaparsec (Parsec)
+import Control.Monad.Combinators.Expr
+
+import Data.Void (Void)
+import Data.Text(Text)
+import Text.Megaparsec.Char (char, space, string, space1)
+import Data.Functor (($>))
+import Control.Applicative (Alternative((<|>), many))
+import Data.Foldable (asum)
+
 data Op = And | Or | Impl
+    deriving Eq
 
 (*&) :: MLFormula p r -> MLFormula p r -> MLFormula p r
 (*&) = BinOp And
@@ -14,13 +27,15 @@ data Op = And | Or | Impl
 (*->) :: MLFormula p r -> MLFormula p r -> MLFormula p r
 (*->) = BinOp Impl
 
-data MLFormula p r 
+data MLFormula p r
     = Prop  p
     | Const Bool
     | BinOp Op (MLFormula p r) (MLFormula p r)
     | Not      (MLFormula p r)
     | Box   r  (MLFormula p r)
     | Dmd   r  (MLFormula p r)
+
+deriving instance (Eq p, Eq r) => Eq (MLFormula p r)
 
 instance Show Op where
     show And  = "/\\"
@@ -35,3 +50,32 @@ instance (Show p, Show r) => Show (MLFormula p r) where
     show (Not      f)       = "~(" <> show f <> ")"
     show (Box   r  f)       = "["  <> show r <> "]" <> show f
     show (Dmd   r  f)       = "<"  <> show r <> ">" <> show f
+
+type Parser = Parsec Void Text
+
+lexeme :: Parser a -> Parser a
+lexeme p = p <* many space1
+
+pFormula :: Parser p -> Parser r -> Parser (MLFormula p r)
+pFormula pProp pRel = makeExprParser (lexeme $ pTerm pProp pRel) (operators pRel)
+
+pTerm :: Parser p -> Parser r -> Parser (MLFormula p r)
+pTerm pProp pRel
+    =   char '(' *> pFormula pProp pRel <* char ')'
+    <|> Prop        <$> pProp
+    <|> Const True  <$ char '1'
+    <|> Const False <$ char '0'
+    <|> Not         <$> (lexeme (char '~') *> pTerm pProp pRel)
+    <|> Box         <$> lexeme (char '[' *> pRel <* char ']') <*> pTerm pProp pRel
+    <|> Dmd         <$> lexeme (char '<' *> pRel <* char '>') <*> pTerm pProp pRel
+
+operators :: Parser r -> [[Operator Parser (MLFormula p r)]]
+operators pRel = [
+        -- [ Prefix $ Not <$  lexeme (char '~')
+        -- , Prefix $ Box <$> lexeme (char '[' *> pRel <* char ']')
+        -- , Prefix $ Dmd <$> lexeme (char '<' *> pRel <* char '>')
+        -- ],
+        [ InfixL $ BinOp And  <$ lexeme (string "/\\")],
+        [ InfixL $ BinOp Or   <$ lexeme (string "\\/")],
+        [ InfixR $ BinOp Impl <$ lexeme (string "->")]
+    ]
